de9000e4408094f86360ad717e933da869bc2b2e
Sam Poder <sam.r.poder@gmail.com>
2023-09-16 11:37:27 -0400

    Working Uploads

--- next/src/pages/api/upload.ts
+++ next/src/pages/api/upload.ts
@@ -5,32 +5,25 @@ export default async function handler(
 	request: NextApiRequest,
 	response: NextApiResponse,
 ) {
-	const body = request.body;
- 
+	const body = JSON.parse(request.body); 
 	try {
 		const jsonResponse = await handleUpload({
 			body,
 			request,
 			onBeforeGenerateToken: async (
 				pathname: string,
-				/* clientPayload?: string, */
 			) => {
 				return {
 					allowedContentTypes: ['image/jpeg', 'image/png', 'image/gif']
 				};
 			},
 			onUploadCompleted: async ({ blob, tokenPayload }) => {
-				// Get notified of client upload completion
-				// ⚠️ This will not work on `localhost` websites,
-				// Use ngrok or similar to get the full upload flow
- 
 				console.log('blob upload completed', blob, tokenPayload);
 			},
 		});
- 
 		return response.status(200).json(jsonResponse);
 	} catch (error) {
-		// The webhook will retry 5 times waiting for a 200
+		console.error(error)
 		return response.status(400).json({ error: (error as Error).message });
 	}
 }
