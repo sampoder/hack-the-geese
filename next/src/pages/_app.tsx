f6bd930c025f14e45f665923629ed960873deb12
Sam Poder <sam.r.poder@gmail.com>
2023-09-17 03:38:24 -0400

    Update _app.tsx

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Toaster } from 'react-hot-toast';
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Hack The Geese</title>
+      </Head>
       <Component {...pageProps} />
       <Toaster />
     </>
