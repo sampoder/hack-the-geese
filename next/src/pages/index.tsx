d73105349f6a3b86a961d50de96d1cd1fab22314
Sam Poder <sam.r.poder@gmail.com>
2023-09-17 00:13:40 -0400

    Move Geese to Index

--- next/src/pages/index.tsx
+++ next/src/pages/index.tsx
@@ -33,8 +33,7 @@ export default function Home() {
       if (message.action == "new_player_connected") {
         if (message.origin == scannedCode) {
           setUser(scannedCode || "");
-          // setGameState("geese")
-          setGameState("ready");
+          setGameState("geese")
         }
       } else if (message.action == "player_connected") {
         if (message.origin == scannedCode) {
@@ -367,6 +366,8 @@ export default function Home() {
           <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
         </main>
       );
+    case "geese":
+      return <GeeseComponent />
     case "playing":
       return (
         <main
@@ -517,6 +518,73 @@ export default function Home() {
       );
   }
 }
+
+const randomHexColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+const changeColorOfGroups = (groupIds:any) => {
+  groupIds.forEach((id:any) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.childNodes.forEach((child) => {
+        if (child instanceof Element) {
+          if (id === 'white') {
+            child.setAttribute('fill', '#FFFFFF');
+          } else {
+            child.setAttribute('fill', randomHexColor());
+          }
+        }
+      });
+    }
+  });
+};
+
+const changeBackgroundColor = () => {
+  document.body.style.backgroundColor = randomHexColor();
+};
+
+const GeeseComponent = () => {
+  useEffect(() => {
+    fetch('geese.svg')
+      .then(response => response.text())
+      .then(data => {
+        const container = document.getElementById('geese-container');
+        if (container) {
+          container.innerHTML = data;
+          
+          // Change colors and background immediately upon loading
+          changeColorOfGroups(['fur1', 'fur2', 'shadow', 'white', 'feet', 'svg-background']);
+          changeBackgroundColor();
+          
+        }
+      });
+  }, []);
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+   <button      
+    onClick={() => { 
+      changeColorOfGroups(['fur1', 'fur2', 'shadow', 'white', 'feet', 'svg-background']); 
+      changeBackgroundColor();
+    }}
+      >
+        Generate Geese
+      </button>
+      
+      <div id="geese-container" style={{ backgroundColor: 'white' }}></div>
+  
+      <Link href="/gamestart">
+        <button>Start Game</button>
+      </Link>
+    </div>
+  );
+};
 
 const ViewFinder = (props: any) => (
   <div
